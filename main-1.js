5d5ca95a63e11a83d3cc797279a9da99c03c0781
Learn1 <153932640+masihPemula5@users.noreply.github.com>
2024-09-01 23:17:00 +0700

    Add files via upload

---
+++ main-1.js
@@ -0,0 +1,17 @@
+particlesJS.load('particles-js', 'particle.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
+
+setTimeout(() => {
+  
+  Swal.fire({
+    title: "Selamat datang di linktree saya!",
+    text: "maaf kalo jelek 🙏🏼😁",
+    icon: "information",
+    confirmButtonText: "Gpp",
+    confirmButtonColor: "#278cff",
+
+  });
+}, 3000);
+
+    
